27b3e54fac8611529773ad1c20e8c7aaaecdccec
Ronnie <rin.vitae@gmail.com>
2021-08-24 03:11:39 -0800

    revision

--- src/main/java/com/mailsac/api/App.java
+++ src/main/java/com/mailsac/api/App.java
@@ -5,30 +5,32 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class App {
+    
+    private static String mailsacAPIKey = "";
+    private static String mailsacToAddress = "";
+    private static String mailsacFromAddress = "";
 
-    static String mailsacAPIKey = "";
-    static String mailsacToAddress = "";
-    static String mailsacFromAddress = "";
-    static String mailsacSubject = "";
-    static String mailsacText = "string";
+    private static String mailsacSubject = "Hello!"; 
+    private static String mailsacText = "Check out https://example.com";
+    private static String mailsacHTML = "Check out <a href='https://example.com'>My website</a>";
 
-    static void sendMail() throws UnirestException {
-        HttpResponse response = Unirest.post("https://mailsac.com/api/outgoing-messages")
-        .header("content-type", "application/json")
-        .header("Mailsac-Key", String.format("%s", mailsacAPIKey))
-        .body(String.format("{\"to\":\"%s\",\"from\":\"%s\",\"subject\":\"%s\",\"text\":\"%s\"}", mailsacToAddress, mailsacFromAddress, mailsacSubject, mailsacText))
-        .asString();
-    }
-
-    static void deleteMail() throws UnirestException {
+    static void purgeInbox() throws UnirestException {
         HttpResponse response = Unirest.delete(String.format("https://mailsac.com/api/addresses/%s/messages", mailsacToAddress))
         .header("Mailsac-Key", String.format("%s", mailsacAPIKey))
         .asString();
     }
 
-    public static void main(String[] args) throws UnirestException {
-        sendMail();
-        deleteMail();
+    static void sendMail() throws UnirestException {
+        HttpResponse response = Unirest.post("https://mailsac.com/api/outgoing-messages")
+        .header("content-type", "application/json")
+        .header("Mailsac-Key", String.format("%s", mailsacAPIKey))
+        .body(String.format("{\"to\":\"%s\",\"from\":\"%s\",\"subject\":\"%s\",\"text\":\"%s\",\"html\":\"%s\"}", mailsacToAddress, mailsacFromAddress, mailsacSubject, mailsacText, mailsacHTML))
+        .asString();
+
     }
 
+    public static void main(String[] args) throws UnirestException {
+        sendMail();
+        purgeInbox();
+    }
 }
