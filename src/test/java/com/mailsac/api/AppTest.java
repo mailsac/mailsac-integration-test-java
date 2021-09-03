5d1a4dd12b24f3224574b6d9db95f927235f3d54
Ronnie <rin.vitae@gmail.com>
2021-09-02 16:58:29 -0800

    revision

--- src/test/java/com/mailsac/api/AppTest.java
+++ src/test/java/com/mailsac/api/AppTest.java
@@ -12,7 +12,6 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.Transport;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.mail.internet.InternetAddress;
 
 import com.mashape.unirest.http.HttpResponse;
